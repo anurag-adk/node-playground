e439e8c001a1b8195010f7cbbe501a984dbdf771
Anurag Adhikari <anuragadhikari5276@gmail.com>
2024-10-05 01:55:26 +0545

    work with file system

---
+++ 03-fileSystem.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const fs = require("fs");
+const app = express();
+const PORT = 8080;
+
+app.get("/", (req, res) => {
+  res.status(200).send("Welcome! This is Landing Page");
+});
+
+app.get("/read", (req, res) => {
+  fs.readFile("./file.txt", "utf8", (err, data) => {
+    if (err) res.send("Cannot Read File!");
+    res.send(data);
+  });
+});
+
+app.get("/write", (req, res) => {
+  data = "Honda";
+  fs.writeFile("./file.txt", data, (err) => {
+    if (err) res.send("Cannod Write to File!");
+    res.send("Successfully Written");
+  });
+});
+
+app.get("/append", (req, res) => {
+  data = "Audi";
+  fs.appendFile("./file.txt", " " + data, (err) => {
+    if (err) res.send("Cannot Append to File!");
+    res.send("Successfully Appended");
+  });
+});
+
+app.listen(PORT, () => {
+  console.log("Server started at port: " + PORT);
+});
